bea2aa0c00d1f51c9fc2fc0ab1304bdfd21c261b
Aaron Augsburger <aaugsburger@clearcover.com>
2021-05-09 17:08:37 -0600

    Created SeaCreature and enhanced runner to include a list of SeaCreatures

---
+++ src/main/SeaCreature.java
@@ -0,0 +1,17 @@
+public class SeaCreature {
+    public void method1(){
+        System.out.println("creature 1");
+    }
+
+    public void method2(){
+        System.out.println("creature2");
+    }
+
+    public void method3(){
+        System.out.println("creature 3");
+    }
+
+    public String toString(){
+        return "ocean-dwelling";
+    }
+}
--- src/main/SeaCreatureRunner.java
+++ src/main/SeaCreatureRunner.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SeaCreatureRunner {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SeaCreature[] elements = {
+                new SeaCreature(),
+                new SeaCreature(),
+                new SeaCreature(),
+                new SeaCreature(),
+        };
+
+        List<String> list = new ArrayList<>();
+        for (SeaCreature element : elements) {
+            System.out.println(element);
+            element.method1();
+            element.method2();
+            System.out.println();
+        }
     }
 }
