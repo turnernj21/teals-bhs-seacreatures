5be22aef4dd9ea726a66b74d4d78e35299d85d6e
turnernj21 <nichot@mich.edu>
2021-05-13 08:32:23 -0400

    I'd like all the numbers 9s you have.

---
+++ src/main/creatures/NJT_Otter.java
@@ -0,0 +1,8 @@
+package creatures;
+
+public class NJT_Otter extends AquaticMammal {
+    public void method2() {System.out.println("Paws:"); }
+    public String toString() {
+        return "Beach Dweller";
+    }
+}
